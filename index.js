bcfc84223e4d7628a1fb868b19b7d4a311b7fdbe
Adailsonacj <adailsonacj@live.com>
2019-03-07 10:51:02 -0300

    Trabalhando com botões

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Text, AppRegistry, View } from 'react-native'
+import { Text, AppRegistry, View, Button, TouchableOpacity } from 'react-native'
 
 const Styles = {
     estiloTexto: {
@@ -9,7 +9,7 @@ const Styles = {
         color: '#fff',
         fontWeight: 'bold'
     },
-    estiloView:{
+    estiloView: {
         backgroundColor: 'skyblue',
         height: 300,
         justifyContent: 'flex-end',
@@ -21,15 +21,48 @@ const Styles = {
         padding: 10,
         color: '#fff',
         fontWeight: 'bold'
+    },
+    principal: {
+        paddingTop: 40
+    },
+
+    textoBotao: {
+        fontSize: 15,
+        padding: 10,
+        color: 'skyblue',
+        fontWeight: 'bold'
+    },
+    botao: {
+        backgroundColor: 'white',
+        alignItems: 'center',
+        borderColor: 'skyblue',
+        borderWidth: 3,
+        borderRadius: 8
     }
 }
 
+const pressButton = () => {
+    alert('Botão Pressionado')
+}
+
 const App = () => {
-    const {estiloTexto, estiloView, estilo2} = Styles
+    const { estiloTexto, estiloView, estilo2, principal, botao, textoBotao } = Styles
     return (
+        /*
         <View style={estiloView}>
             <Text style={estiloTexto}>Frases do Dia</Text>
             <Text style={estilo2}>Frases do Dia</Text>
+        </View>
+        */
+        <View style={principal}>
+            <Button title="Clique aqui"
+                onPress={pressButton}
+                color="#841584"
+                accessibilityLabel="Learn more about this purple button"
+            />
+            <TouchableOpacity style={botao} onPress={pressButton}>
+                <Text style={textoBotao}>CLIQUE AQUI</Text>
+            </TouchableOpacity>
         </View>
     )
 }
