68372dfdf6cbde9d12edd8b8e63c0d828fd29047
Adailsonacj <adailsonacj@live.com>
2019-03-07 00:05:44 -0300

    second commit

--- index.js
+++ index.js
@@ -1,20 +1,36 @@
 import React from 'react'
-import {Text, AppRegistry} from 'react-native'
+import { Text, AppRegistry, View } from 'react-native'
 
 const Styles = {
-    esiloTexto:{
+    estiloTexto: {
         fontSize: 20,
         backgroundColor: '#87CEEB',
         padding: 10,
-        textAlign: 'center',
+        color: '#fff',
+        fontWeight: 'bold'
+    },
+    estiloView:{
+        backgroundColor: 'skyblue',
+        height: 300,
+        justifyContent: 'flex-end',
+        alignItems: 'center'
+    },
+    estilo2: {
+        fontSize: 10,
+        backgroundColor: '#87CEEB',
+        padding: 10,
         color: '#fff',
         fontWeight: 'bold'
     }
 }
 
 const App = () => {
+    const {estiloTexto, estiloView, estilo2} = Styles
     return (
-        <Text style={Styles.esiloTexto}>Frases do Dia</Text>
+        <View style={estiloView}>
+            <Text style={estiloTexto}>Frases do Dia</Text>
+            <Text style={estilo2}>Frases do Dia</Text>
+        </View>
     )
 }
 
