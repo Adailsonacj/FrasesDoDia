fb191eb68d5c5aab9fb3895cb8bb9093ac13bc8e
Adailsonacj <adailsonacj@live.com>
2019-03-08 00:41:07 -0300

    Imagens na aplicação

--- index.js
+++ index.js
@@ -1,68 +1,21 @@
 import React from 'react'
-import { Text, AppRegistry, View, Button, TouchableOpacity } from 'react-native'
+import { Text, AppRegistry, View, Image } from 'react-native'
 
 const Styles = {
-    estiloTexto: {
-        fontSize: 20,
-        backgroundColor: '#87CEEB',
-        padding: 10,
-        color: '#fff',
-        fontWeight: 'bold'
-    },
-    estiloView: {
-        backgroundColor: 'skyblue',
-        height: 300,
-        justifyContent: 'flex-end',
-        alignItems: 'center'
-    },
-    estilo2: {
-        fontSize: 10,
-        backgroundColor: '#87CEEB',
-        padding: 10,
-        color: '#fff',
-        fontWeight: 'bold'
-    },
+
     principal: {
         paddingTop: 40
-    },
-
-    textoBotao: {
-        fontSize: 15,
-        padding: 10,
-        color: 'skyblue',
-        fontWeight: 'bold'
-    },
-    botao: {
-        backgroundColor: 'white',
-        alignItems: 'center',
-        borderColor: 'skyblue',
-        borderWidth: 3,
-        borderRadius: 8
     }
 }
 
-const pressButton = () => {
-    alert('Botão Pressionado')
-}
-
 const App = () => {
-    const { estiloTexto, estiloView, estilo2, principal, botao, textoBotao } = Styles
+    const { principal } = Styles
     return (
-        /*
-        <View style={estiloView}>
-            <Text style={estiloTexto}>Frases do Dia</Text>
-            <Text style={estilo2}>Frases do Dia</Text>
-        </View>
-        */
         <View style={principal}>
-            <Button title="Clique aqui"
-                onPress={pressButton}
-                color="#841584"
-                accessibilityLabel="Learn more about this purple button"
+            <Image source={require('./imgs/uvas.png')} />
+            <Image source={{ uri: 'https://www.festisite.com/static/partylogo/img/logos/nsa.png' }}
+                style={{ width: 200, height: 200 }}
             />
-            <TouchableOpacity style={botao} onPress={pressButton}>
-                <Text style={textoBotao}>CLIQUE AQUI</Text>
-            </TouchableOpacity>
         </View>
     )
 }
